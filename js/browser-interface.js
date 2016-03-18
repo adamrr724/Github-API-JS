b419196ef6d2bc3c7d618d81bd1c75e460147d2c
Adam Ross Russell <student@epicodus.com>
2016-03-18 11:56:31 -0700

    add get Repos function

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,8 +1,29 @@
-// var object = require('./../js/object.js').object;
-// 
-// $(document).ready(function(){
-//   $('#location').submit(function(event){
-//     event.preventDefault();
-//
-//     });
-//   });
+var getRepos = require('./../js/github-interface.js').getRepos;
+var getDetails = require('./../js/github-interface.js').getDetails;
+
+
+$(document).ready(function(){
+  $('#getRepos').click(function(event){
+    event.preventDefault();
+    repository = $('#repository').val();
+    getRepos(repository);
+    $('#details').show();
+    });
+
+  $('#details').click(function(event){
+    event.preventDefault();
+    repository = $('#repository').val();
+    getDetails(repository);
+    $('.details').show();
+    $('#details').hide();
+    $('#hide').show();
+
+    });
+
+  $('#hide').click(function(event){
+    event.preventDefault();
+    $('.details').hide();
+    $('#details').show();
+    $('#hide').hide();
+    });
+  });
---
+++ js/github-interface.js
@@ -0,0 +1,55 @@
+var apiKey = require('./../.env').apiKey;
+
+exports.getRepos = function(username){
+  $('.showOwner').empty();
+  $('.showRepos').empty();
+
+  $.get('https://api.github.com/users/' + username  + '?access_token=' + apiKey).then(function(response){
+    $('.showOwner').append("<h1>" + response.login + "'s Repositories:</h1>");
+
+  $.get('https://api.github.com/users/' + username  + '/repos?access_token=' + apiKey).then(function(respositories){
+    respositories.forEach(function(repo) {
+      $('.showRepos').append("<h4><a href=" + repo.html_url + ">" + repo.name + "</a></h4>");
+    });
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
+  });
+};
+
+exports.getDetails = function(username) {
+  $('.details').empty();
+
+  $.get('https://api.github.com/users/' + username  + '?access_token=' + apiKey).then(function(user){
+    console.log(user);
+
+    if (user.name !== null){
+      $('.details').append("<p>Name: " + user.name + "</p>");
+    } else {  $('.details').append("This user has not set up a name detail.<br>");
+    }
+
+    if (user.location !== null){
+      $('.details').append("<p>Location: " + user.location + "</p>");
+    } else {
+      $('.details').append("This user has not set up a location detail.<br>");
+    }
+
+    if (user.blog !== null){
+      $('.details').append("<p>Blog: <a href=" + user.blog + ">" + user.blog + "</a></p>");
+    } else {
+      $('.details').append("This user has not set up a blog link.<br>");
+    }
+
+    if (user.company !== null){
+      $('.details').append("<p>Company: " + user.company + "</p>");
+    } else {
+      $('.details').append("This user has not set up a company name.<br>");
+    }
+
+    if (user.email !== null){
+      $('.details').append("<p> Email: " + user.email + "</p>");
+    } else {
+      $('.details').append("This user has not set up an email.<br>");
+    }
+});
+};
