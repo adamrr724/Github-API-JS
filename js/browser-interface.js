792d33071be6392f3cd3ea6b5083274c54c4870a
Adam Ross Russell <student@epicodus.com>
2016-03-18 11:58:01 -0700

    add browser interface functionality

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -5,15 +5,19 @@ var getDetails = require('./../js/github-interface.js').getDetails;
 $(document).ready(function(){
   $('#getRepos').click(function(event){
     event.preventDefault();
+
     repository = $('#repository').val();
     getRepos(repository);
+
     $('#details').show();
     });
 
   $('#details').click(function(event){
     event.preventDefault();
+
     repository = $('#repository').val();
     getDetails(repository);
+
     $('.details').show();
     $('#details').hide();
     $('#hide').show();
@@ -22,6 +26,7 @@ $(document).ready(function(){
 
   $('#hide').click(function(event){
     event.preventDefault();
+
     $('.details').hide();
     $('#details').show();
     $('#hide').hide();
